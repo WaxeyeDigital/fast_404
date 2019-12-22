17a7508599f55235d896b48b656c23939e7d4f2c
rpayanm <rpayanm@2209454.no-reply.drupal.org>
2019-12-22 11:08:28 -0800

    Issue #3095634 by rpayanm, bonus: Add defaultTheme in tests extending BrowserTestBase

--- tests/src/Functional/Fast404ExtensionTest.php
+++ tests/src/Functional/Fast404ExtensionTest.php
@@ -12,6 +12,11 @@ use Drupal\Tests\BrowserTestBase;
 class Fast404ExtensionTest extends BrowserTestBase {
 
   /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
    * Modules to install.
    *
    * @var array
